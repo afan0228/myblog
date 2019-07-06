347058986a66d446c69345a35eae3ecd265f842b
fananying <784481872@qq.com>
2019-07-07 03:39:52 +0800

    add photos

--- tools.py
+++ tools.py
@@ -131,7 +131,7 @@ def handle_photo():
             list_info[-1]['arr']['type'].append('image')
     list_info.reverse()  # 翻转
     final_dict = {"list": list_info}
-    with open("https://github.com/afan0228/myblog/blob/master/source/photos/ins.json","w") as fp:
+    with open("https://github.com/afan0228/myblog/tree/master/source/photos/ins.json","w") as fp:
         json.dump(final_dict, fp)
 
 def cut_photo():
