231ec71f6372280276b10396789d0f56789fc0e7
fananying <784481872@qq.com>
2019-07-07 04:04:24 +0800

    add photos

--- source/photos/ins.js
+++ source/photos/ins.js
@@ -116,7 +116,7 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://raw.githubusercontent.com/afan0228/myblog/master/min_photos/' + data.link[i];
+          var minSrc = 'https://raw.githubusercontent.com/afan0228/myblog/master/min_photos/' + data.link[i] + '.min.jpg';
           var src = 'https://raw.githubusercontent.com/afan0228/myblog/master/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
@@ -124,7 +124,7 @@
 
           liTmpl += '<figure class="thumb" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject">\
                 <a href="' + src + '" itemprop="contentUrl" data-size="640x640" data-type="' + type + '" data-target="' + target + '">\
-                  <img class="reward-img" data-type="' + type + '" data-src="' + minSrc + '" src="/img/empty.png" itemprop="thumbnail" onload="lzld(this)">\
+                  <img class="reward-img" data-type="' + type + '" data-src="' + minSrc + '" src="/assets/img/empty.png" itemprop="thumbnail" onload="lzld(this)">\
                 </a>\
                 <figcaption style="display:none" itemprop="caption description">' + data.text[i] + '</figcaption>\
             </figure>';
