1c7ebfc1267438691c999bf5412e98cefa9a0610
fananying <784481872@qq.com>
2019-07-07 03:37:50 +0800

    add photos

--- source/photos/ins.js
+++ source/photos/ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://github.com/afan0228/myblog/master/min_photos/' + data.link[i];
-          var src = 'https://github.com/afan0228/myblog/master/photos/' + data.link[i];
+          var minSrc = 'https://github.com/afan0228/myblog/tree/master/min_photos/' + data.link[i];
+          var src = 'https://github.com/afan0228/myblog/tree/master/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '.jpg';
